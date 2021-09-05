ca8326882e62951065b035d06d82285d2e525ae2
Alex Raeder <alex@raeder.ru>
2021-09-05 15:09:54 +0300

    Some changes in actons

--- util/constants.go
+++ util/constants.go
@@ -8,8 +8,16 @@ var (
 	VERSION_NUMBER = fmt.Sprintf("%.02f", 1.0)
 	VERSION        = VERSION_NUMBER
 	COMMIT         = ""
+	BUILD_TIME     = ""
 )
 
 func Version() string {
-	return VERSION + " " + COMMIT
+	v := VERSION
+	if COMMIT != "" {
+		v = v + "@" + COMMIT
+	}
+	if BUILD_TIME != "" {
+		v += " (build at " + BUILD_TIME + ")"
+	}
+	return v
 }
